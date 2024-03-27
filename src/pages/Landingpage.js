75c9d9cde03302350911e5732c9b84d99ddcea6e
OtroAlien <flopysueiro1@gmail.com>
2024-03-27 11:03:21 -0300

    Instala dependencias de Font Awesome

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -1,12 +1,14 @@
 // Landing.js
 import React from 'react';
 import { BannerProductos } from '../components/BannerInfo';
+import WhatsappButton from '../components/WhatsappButton';
 
 const Landing = () => {
   return (
     <div className="App">
       {/* Muestra los banners definidos en BannerProductos */}
       <BannerProductos />
+      <WhatsappButton phoneNumber="123456789" message="Hola, tengo una pregunta sobre..." />
     </div>
   );
 };
